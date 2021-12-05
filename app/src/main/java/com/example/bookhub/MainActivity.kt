0ddabb48d30bf0f4764d1abf60ff0c033be1b870
Sudip <sudipbiswas322@gmail.com>
2021-12-05 17:27:53 +0530

    1st

---
+++ app/src/main/java/com/example/bookhub/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.bookhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
+        toolbar = findViewById(R.id.toolbar)
+        frameLayout = findViewById(R.id.frame)
+        navigationView = findViewById(R.id.navigationView)
+
+        setUpToolbar()
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.dashboard ->{
+                    Toast.makeText(this,"Clicked on Dashboard", Toast.LENGTH_LONG).show()
+                }
+                R.id.favorites ->{
+                    Toast.makeText(this,"Clicked on Favourites", Toast.LENGTH_LONG).show()
+                }
+                R.id.profile ->{
+                    Toast.makeText(this,"Clicked on Profile", Toast.LENGTH_LONG).show()
+                }
+                R.id.aboutApp ->{
+                    Toast.makeText(this,"Clicked on About App", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            return@setNavigationItemSelectedListener true
+        }
+    }
+    fun setUpToolbar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Toolbar Title"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if(id == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
